bca6fdfdc04d20b1c9d2a9e9081e37dd2a0b7698
jiangxq <jiangxq@telpo.com>
2018-08-24 10:56:51 +0800

    delete footer and header

--- app/src/main/java/com/jiangxq/filmchina/model/ArticalDetailModel.java
+++ app/src/main/java/com/jiangxq/filmchina/model/ArticalDetailModel.java
@@ -49,6 +49,7 @@ public class ArticalDetailModel {
                     articalDoc.getElementsByClass("breadcrumbs hidden-xs").remove();
                     articalDoc.getElementsByClass("nt_search_inner nt_search_inner_down hidden-xs").remove();
                     articalDoc.getElementsByClass("nt-header navbar mobile-style02").remove();
+                    articalDoc.getElementsByClass("mobile-navbar row hidden-md hidden-lg").remove();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
